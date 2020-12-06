3cd395b0b12641669674784886a6c394f333afa7
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-05 21:07:32 -0500

    enable backend and add slug field for markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+    const { createNodeField } = actions;
+    console.log(node.internal.type)
+    if(node.internal.type === `MarkdownRemark`) {
+        // slug is just a route
+        const slug = createFilePath({ node, getNode })
+        console.log(slug)
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug
+        })
+    }
+}
