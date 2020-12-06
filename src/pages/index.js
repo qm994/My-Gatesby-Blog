b65500bbed81c95a4e04ed8c38cb16e921ce4c24
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-05 20:46:43 -0500

    add data from graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,47 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>Qingyuan's Thoughts</h1>
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {
+          data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <span>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </span>
+              <p>{ node.excerpt }</p>
+            </div>
+          ))
+        }
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+// if u make a `graphql` query, the gatesby smart enough to know apply the query return to the default export
+export const query = graphql`
+  query  {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date
+              description
+            }
+            excerpt
+          }
+        }
+        totalCount
+      }
+    }
+`
